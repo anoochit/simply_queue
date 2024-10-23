b063199cb4744409e85477b82471978119a8f743
Anuchit Chalothorn <anoochit@gmail.com>
2024-10-23 14:47:56 +0700

    add blank screen, middleware and leadback feature

---
+++ simply_queue_signage/lib/app/middlewares/route_guard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:simply_queue_signage/serverpod.dart';
+
+import '../routes/app_pages.dart';
+
+class RouteGuard extends GetMiddleware {
+  @override
+  RouteSettings? redirect(String? route) {
+    final auth = sessionManager.isSignedIn;
+    if (auth) {
+      return null;
+    } else {
+      return const RouteSettings(name: Routes.SIGNIN);
+    }
+  }
+}
--- simply_queue_signage/lib/main.dart
+++ simply_queue_signage/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:simply_queue_signage/serverpod.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initServerPodClient();
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Signage",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
